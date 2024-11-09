fe502f75fb684639dcf94878943caa62c7b64ed7
Dmitriy B. <ybdapw@gmail.com>
2024-11-09 14:19:49 +0500

    Init

---
+++ WeightedRandom/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace WeightedRandom;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var probabilities = new Dictionary<string, decimal>
+        {
+            { "A", 0.7m },
+            { "B", 0.2m },
+            { "C", 0.1m }  // The rest for C
+        };
+
+        var randomResults = new List<string>();
+        
+        // Run the function multiple times to see the weighted randomness
+        for (int i = 0; i < 1000; i++)
+        {
+            randomResults.Add(WeightedRandom.GetRandom(probabilities));
+        }
+        
+        Console.WriteLine(JsonSerializer.Serialize(WeightedRandom.CountDifferentItems(randomResults), new JsonSerializerOptions { WriteIndented = true }));
+    }
+}
---
+++ WeightedRandom/WeightedRandom.cs
@@ -0,0 +1,43 @@
+﻿namespace WeightedRandom;
+
+// Thread safe
+public class WeightedRandom
+{
+    // Generic method to select an item based on given probabilities
+    public static T GetRandom<T>(IDictionary<T, decimal> probabilities)
+    {
+        // Sum of all the weights
+        decimal totalWeight = 0;
+        foreach (var probability in probabilities.Values)
+        {
+            totalWeight += probability;
+        }
+
+        // Generate a random number in the range [0, totalWeight]
+        decimal randomValue = new decimal(Random.Shared.NextDouble()) * totalWeight;
+
+        // Determine which item the random value corresponds to
+        foreach (var item in probabilities)
+        {
+            randomValue -= item.Value;
+            if (randomValue <= 0)
+            {
+                return item.Key;
+            }
+        }
+
+        throw new Exception("Probabilities are invalid");
+    }
+
+    public static Dictionary<T, int> CountDifferentItems<T>(IEnumerable<T> items) where T : notnull
+    {
+        var dictionary = new Dictionary<T, int>();
+
+        foreach (var item in items.Where(item => !dictionary.TryAdd(item, 1)))
+        {
+            dictionary[item] += 1;
+        }
+
+        return dictionary;
+    }
+}
